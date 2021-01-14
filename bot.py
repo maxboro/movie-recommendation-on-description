5aa4f98d0691e3ad645f1b2f7879b650ae512b61
Max B <maksym_borovyk@ukr.net>
2021-01-14 18:12:34 +0200

    Added tags extraction function for Movie class

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+import spacy
 import numpy as np
 import pandas as pd
 
@@ -16,6 +17,24 @@ class Movie:
         self.avg_vote = parameter_dict['avg_vote']
         self.votes = parameter_dict['votes']
         self.title = parameter_dict['title']
+        self.tags = self.__tags_extraction(self.description)
+    
+    def __tags_extraction(self, text: str) -> set:
+        doc = nlp(text)
+        tags = {
+                token.text.lower() 
+                for token in doc 
+                    if (token.text.lower() not in nlp.Defaults.stop_words) 
+                    and (not token.is_punct)
+                    and (not token.is_digit)
+                }
+        noun_chunks = {
+                token.text.lower()
+                for token in doc.noun_chunks
+                    if token.text.lower() not in nlp.Defaults.stop_words
+                }
+        tags.update(noun_chunks)
+        return tags
     
 class MovieCollection :
     
